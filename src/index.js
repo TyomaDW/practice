a2e2dd939e8f18a0b9b0958e0e49687f3decd65a
Tyoma <risendrums777@gmail.com>
2022-07-01 17:39:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Header from "./Components/Header";
-import ButonsRow from "./Components/ButonsRow";
+import Header from "./components/Header";
+import ButonsRow from "./components/ButonsRow";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
