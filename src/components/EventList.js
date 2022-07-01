51ace8d70f11e5dc5e7993cbfb5c5d3f8b705d7f
Tyoma <risendrums777@gmail.com>
2022-07-01 20:27:05 +0300

    Create EventList.js

---
+++ src/components/EventList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function EventList() {
+  return (
+    <div className="container mx-auto bg-slate-300">
+      <p className="">Lorem ipsum dolor sit amet</p>
+      <button>•••</button>
+      <div>{new Date().toString()}</div>
+    </div>
+  );
+}
