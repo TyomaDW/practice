e7683bc17435748e60499439d5eb132657e51ab9
Tyoma <risendrums777@gmail.com>
2022-07-01 17:58:00 +0300

    Update ButonsRow.js

--- src/Components/ButonsRow.js
+++ src/Components/ButonsRow.js
@@ -8,13 +8,13 @@ export default function ButonsRow() {
         <div className="flex flex-col bg-slate-300 rounded-lg px-4">
           <button
             type="button"
-            class="active:text-white active:bg-blue-600 font-semibold rounded-full text-xl py-2 text-center mb-1.5 mt-3"
+            className="text-white bg-blue-600 font-semibold rounded-full text-xl py-2 text-center mb-1.5 mt-3"
           >
             Published
           </button>
           <button
             type="button"
-            class="active:text-white active:bg-blue-600 font-semibold rounded-full text-xl py-2 text-center mb-3"
+            className="active:text-white active:bg-blue-600 font-semibold rounded-full text-xl py-2 text-center mb-3"
           >
             Unpublished
           </button>
@@ -23,7 +23,7 @@ export default function ButonsRow() {
       <div className="fixed inset-x-9 bottom-0 lg:static">
         <button
           type="button"
-          class="flex items-center z-10 text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:ring-4 focus:ring-green-300 font-semibold rounded-full text-xl px-20 lg:px-8 py-4 text-center mb-5"
+          className="flex items-center z-10 text-white bg-green-600 hover:bg-green-500 focus:ring-green-300 font-semibold rounded-full text-xl px-20 lg:px-8 py-4 text-center mb-5"
         >
           <img className="mr-4" src={add} alt="icon" />
           Add Event
