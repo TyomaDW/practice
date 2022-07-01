40c32bd9bb84a61ed5f46bd474e83829b082e264
Tyoma <risendrums777@gmail.com>
2022-07-01 20:27:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Header from "./components/Header";
-import ButonsRow from "./components/ButonsRow";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
-    <ButonsRow />
+    <App />
   </React.StrictMode>
 );
 
