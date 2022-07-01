cc9b1242489e5d5c4e6e5bf748f16683beb16182
Tyoma <risendrums777@gmail.com>
2022-07-01 18:54:02 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,20 +5,20 @@ import icon from "./Icons/icon.svg";
 export default function Header() {
   return (
     <>
-      <div className="mx-auto px-4 pt-3.5 pb-4 container lg:pr-21 lg:px-4 lg:flex lg:justify-between">
+      <div className="mx-auto px-4 pt-3.5 pb-4 container sm:pr-21 sm:px-4 sm:flex sm:justify-between">
         <div className="block">
-          <h1 className="font-semibold text-center text-4xl lg:py-5">
+          <h1 className="font-semibold text-center text-4xl sm:py-5">
             Event Manager
           </h1>
         </div>
-        <div className="lg:flex">
-          <div className="flex justify-center pt-4 pb-3.5 lg:items-center">
+        <div className="sm:flex">
+          <div className="flex justify-center pt-4 pb-3.5 sm:items-center">
             <img className="mr-4" src={icon} alt="icon" />
-            <h2 className="mt-1 font-semibold text-xl lg:mr-8">
+            <h2 className="mt-1 font-semibold text-xl sm:mr-8">
               Select Timezone
             </h2>
           </div>
-          <div className="flex justify-center lg:items-center">
+          <div className="flex justify-center sm:items-center">
             <select
               className="block w-full appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
               name="timezones"
