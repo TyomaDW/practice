dc1e8189c893c198a10fd939d4d8c010b0084745
Tyoma <risendrums777@gmail.com>
2022-07-05 01:59:34 +0300

    Update EventList.js

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -2,10 +2,40 @@ import React from "react";
 
 export default function EventList() {
   return (
-    <div className="container mx-auto bg-slate-300">
-      <p className="">Lorem ipsum dolor sit amet</p>
-      <button>•••</button>
-      <div>{new Date().toString()}</div>
+    <div className="container mx-auto flex flex-col space-y-4 sm:space-y-0 sm:grid sm:grid-cols-3 sm:gap-3">
+      <div className="px-4">
+        <div className="bg-slate-300 rounded-[18px]">
+          <p className="font-semibold text-xl mx-5 pb-10 pt-3">
+            Lorem ipsum dolor sit amet consectetur
+          </p>
+          <div className="flex justify-between mx-5 pb-3.5">
+            <button>•••</button>
+            <p className="font-medium text-base">{new Date().toDateString()}</p>
+          </div>
+        </div>
+      </div>
+      <div className="px-4">
+        <div className="bg-slate-300 rounded-[18px]">
+          <p className="font-semibold text-xl mx-5 pb-10 pt-3">
+            Lorem ipsum dolor sit amet consectetur
+          </p>
+          <div className="flex justify-between mx-5 pb-3.5">
+            <button>•••</button>
+            <p className="font-medium text-base">{new Date().toDateString()}</p>
+          </div>
+        </div>
+      </div>
+      <div className="px-4">
+        <div className="bg-slate-300 rounded-[18px]">
+          <p className="font-semibold text-xl mx-5 pb-10 pt-3">
+            Lorem ipsum dolor sit amet consectetur
+          </p>
+          <div className="flex justify-between mx-5 pb-3.5">
+            <button>•••</button>
+            <p className="font-medium text-base">{new Date().toDateString()}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
