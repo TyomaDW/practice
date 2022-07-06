27e841cdfe402325b5f1e116156aaf6c85049ceb
Tyoma <risendrums777@gmail.com>
2022-07-06 19:45:12 +0300

    add dropdown

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -88,28 +88,6 @@ export default function EventList() {
           </div>
         </div>
       </div>
-      <div className="px-4">
-        <div className="bg-slate-300 rounded-[18px]">
-          <p className="font-semibold text-xl mx-5 pb-10 pt-3">
-            Lorem ipsum dolor sit amet consectetur
-          </p>
-          <div className="flex justify-between mx-5 pb-3.5">
-            <button>•••</button>
-            <p className="font-medium text-base">{new Date().toDateString()}</p>
-          </div>
-        </div>
-      </div>
-      <div className="px-4">
-        <div className="bg-slate-300 rounded-[18px]">
-          <p className="font-semibold text-xl mx-5 pb-10 pt-3">
-            Lorem ipsum dolor sit amet consectetur
-          </p>
-          <div className="flex justify-between mx-5 pb-3.5">
-            <button>•••</button>
-            <p className="font-medium text-base">{new Date().toDateString()}</p>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
